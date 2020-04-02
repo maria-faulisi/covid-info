582a096d50770f0aa6cd44dcb0a67f04d6e7964b
Maria Faulisi <mariafaulisi@marias-mbp.lan>
2020-04-02 17:05:40 -0500

    all flags working

--- public/index.js
+++ public/index.js
@@ -1,4 +1,9 @@
+//load iso data from local file
 const data = Data;
+
+//firebase config
+
+
 //handlebars template for articles
 let confirmed_source = $('#confirmed').html();
 let confirmed_template = Handlebars.compile(confirmed_source);
@@ -12,16 +17,6 @@ const $country_list_container = $('#confirmed-list-container');
 const country_codes = [];
 const flags = [];
 
-function getFlag(country_codes, name){
-  for (var i = country_codes.length - 1; i >= 0; i--) {
-    //console.log(country_codes[i]);
-    if (country_codes[i]['name'] === name) {
-      let code = country_codes['code'];
-      return 'https://www.countryflags.io/' + code + '/flat/64.png';
-    }
-  }
-};
-
 function getAndDisplayData(iso_codes){
   $.ajax({
     url: base_url + corona_summary,
@@ -33,14 +28,17 @@ function getAndDisplayData(iso_codes){
 
         for (var i = rawData.length - 1; i >= 0; i--) {
           if (rawData[i]['TotalConfirmed']) {
+            let country_data = rawData[i]['Country'].replace(/\s+/g, '-');
             confirmed_countries.push({
               country: rawData[i]['Country'],
               confirmed: rawData[i]['TotalConfirmed'],
               deaths: rawData[i]['TotalDeaths'],
-              recovered: rawData[i]['TotalRecovered']
+              recovered: rawData[i]['TotalRecovered'],
+              country_data: country_data,
             });
           } else {
-            no_case_countries.push({country: rawData[i]['Country']});
+            let country_data = rawData[i]['Country'].replace(/\s+/g, '-');
+            no_case_countries.push({country: rawData[i]['Country'], country_data: country_data});
           }
         }
         //reverse order of arrays list alphabetically
@@ -60,10 +58,13 @@ function getAndDisplayData(iso_codes){
       $individual_country.each(function(index, element){
         let $element = $(this);
         let $elementName = $element.data('name');
-
+        console.log($elementName);
+        
         for (var i = iso_codes.length - 1; i >= 0; i--) {
           if ($elementName === iso_codes[i]['name']) {
             $element.find('img').attr('src', 'https://www.countryflags.io/' + iso_codes[i]['code'] + '/flat/64.png');
+          }else if ($elementName === 'Cruise-Ship'|| $elementName === 'Diamond-Princess' || $elementName === 'MS-Zaandam' || $elementName === 'Others'){
+            $element.find('img').attr('src', '../ship.svg').addClass('other-flag');
           }
         }
 
