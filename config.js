90481fd6f400c4f09acc13d1c2dc101559986507
Maria Faulisi <mariafaulisi@marias-mbp.lan>
2020-04-05 10:29:07 -0500

    expose firebase config no work around to hide w/o backend

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyC4IrGLekUP5utlREPB4dkMLvCHi8j5Y-o",
+  authDomain: "covid19-info-and-help.firebaseapp.com",
+  databaseURL: "https://covid19-info-and-help.firebaseio.com",
+  projectId: "covid19-info-and-help",
+  storageBucket: "covid19-info-and-help.appspot.com",
+  messagingSenderId: "333753248869",
+  appId: "1:333753248869:web:3e08241102ed72f13aaa8c",
+  measurementId: "G-FXY2YGZHTS"
+};
